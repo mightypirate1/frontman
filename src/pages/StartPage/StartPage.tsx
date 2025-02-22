ca4d54bda7eab0b3bfa13ab7d0daf8b18551276a
mightypirate1 <yfflan@gmail.com>
2025-02-22 20:30:07 +0100

    tidying

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -1,40 +1,19 @@
 import { useState } from 'react'
-import { Divider, Button, Chip, Skeleton, Toolbar, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography, Box, Drawer, CssBaseline, AppBar, IconButton, Avatar, Tabs, Tab } from '@mui/material'
-import MailIcon from '@mui/icons-material/Mail';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MenuIcon from '@mui/icons-material/Menu';
+import { Toolbar, Typography, Box, AppBar, Avatar, Tab } from '@mui/material'
 import TabContext from '@mui/lab/TabContext';
 import '../../App.css'
 
 import SnakeController from '../../components/Snake/SnakeController';
 import Dbg from '../../components/GameOfLife/Dbg';
-import { List, Inbox, Mail } from '@mui/icons-material';
 import { TabList, TabPanel } from '@mui/lab';
 
 function StartPage() {
-  const appBarWidth = 240;
   const headerHeight = 65;
   const ghAvatar = "https://avatars.githubusercontent.com/u/19390975?s=400&u=1358e735334214fa0a842a63d949fe5363e88494&v=4";
-  const [activePage, setActivePage] = useState("start");
-  
-  const getContents = (activePage: string) => {
-    switch (activePage) {
-      case "snake":
-        return <SnakeController />;
-      case "dbg":
-        return <Dbg />;
-      default:
-        return (
-          <Box>
-            <Typography variant="h4">Here are some toys!</Typography>
-            <Typography variant="h6">Click on the buttons to the left to navigate.</Typography>
-          </Box>
-        );
-    }
-  };
+  const [activeTab, setActiveTab] = useState("start");
 
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setActivePage(newValue);
+  const handleTabChange = (_event: React.SyntheticEvent, selectedTab: string) => {
+    setActiveTab(selectedTab);
   };
 
   return (
@@ -56,9 +35,9 @@ function StartPage() {
         </Toolbar>
       </AppBar>
       <Box component="nav" sx={{ width: "100hh", marginTop: `${headerHeight}px` }}>
-        <TabContext value={activePage}>
+        <TabContext value={activeTab}>
           <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <TabList onChange={handleTabChange} aria-label="lab API tabs example">
               <Tab label="Disclaimer" value="start" />
               <Tab label="Snake" value="snake" />
               <Tab label="Dbg" value="dbg" />
